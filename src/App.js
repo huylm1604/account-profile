7ea071f382a34150e204f3e3f9c665cc43039af3
huylm1604 <huyknight1604@gmail.com>
2020-03-18 21:46:01 +0700

    add table for account profile organization table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 import React from "react";
 import styles from "./App.css";
-import { Row, Col, Divider, Form, Input, Button } from "antd";
+import { Row, Col, Divider, Form, Input, Button, Typography, Table } from "antd";
+
+const { Title } = Typography;
+// dataSource dummie data
+const dataSource = [
+  {
+    key: "1",
+    name: "Tap Doan Huy Map"
+  },
+  {
+    key: "2",
+    name: "Web cua Don Dai Ca"
+  },
+  {
+    key: "3",
+    name: "Orga 3"
+  },
+];
+
+
+
+// table columns
+const columns = [
+  {
+    title: "Organization Name",
+    dataIndex: "name",
+    key: "name",
+    editable: true,
+    render: title => <a>{title}</a>,
+  },
+  {
+    render: () => (
+      <Button type="danger" >Delete</Button>
+    ),
+    
+  }
+];
 
 function App() {
   const formItemLayout = {
@@ -27,11 +63,12 @@ function App() {
   };
   return (
     <div>
-      <Divider orientation="left" className={styles.divider}>
-        Profile Settings
-      </Divider>
-      <Row style={{marginTop : "3%"}}>
+      <Title level={2}>Account profile</Title>
+      <Row style={{ marginTop: "3%" }}>
         <Col span={6} offset={8}>
+          <Divider orientation="left" className={styles.divider}>
+            <Title level={4}>Profile information</Title>
+          </Divider>
           <Form name="register" scrollToFirstError {...formItemLayout}>
             <Form.Item
               name="email"
@@ -50,7 +87,7 @@ function App() {
               <Input />
             </Form.Item>
             <Form.Item
-              name="nickname"
+              name="fullName"
               label="Fullname"
               rules={[
                 {
@@ -62,10 +99,24 @@ function App() {
             >
               <Input />
             </Form.Item>
-
+            <Form.Item {...tailFormItemLayout}>
+              <div>
+                <Button type="primary" htmlType="submit">
+                  Update profile
+                </Button>
+                <Button style={{ marginLeft: "5%" }} htmlType="reset">
+                  Reset
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
+          <Divider orientation="left" className={styles.divider}>
+            <Title level={4}>Password change</Title>
+          </Divider>
+          <Form name="passwordChange" scrollToFirstError {...formItemLayout}>
             <Form.Item
               name="password"
-              label="Password"
+              label="Old Password"
               rules={[
                 {
                   required: true,
@@ -105,15 +156,23 @@ function App() {
             <Form.Item {...tailFormItemLayout}>
               <div>
                 <Button type="primary" htmlType="submit">
-                  Update
+                  Change Password
                 </Button>
-                <Button style={{marginLeft : "5%"}} htmlType="submit">Reset</Button>
               </div>
             </Form.Item>
           </Form>
         </Col>
       </Row>
+      <Divider orientation="left" className={styles.divider}>
+            <Title level={4}>My Organization</Title>
+          </Divider>
+    <Table
+      pagination={{ position: "both" }}
+      columns={columns}
+      dataSource={dataSource}
+    ></Table>
     </div>
+    
   );
 }
 
